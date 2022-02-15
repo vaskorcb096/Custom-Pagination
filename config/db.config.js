6deda1e554606821ec2b7812629fdee6c27a8d42
vaskorcb096e <vchakroborty096@gmaile.com>
2022-02-15 08:29:09 -0600

    updated

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "paginationDB",
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASSWORD,
+    DB: process.env.DB_NAME,
     dialect: "mysql",
    
   };
